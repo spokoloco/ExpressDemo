a07f03ccd8ccea2e426c26a60b2bce2715781370
Brett <brett.brewster.13@gmail.com>
2019-08-28 09:39:36 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+    res.send('Hello WorlD!');
+});
+
+app.get('/m/:variable/:variable2', function(req,res){
+    var variable = req.params.variable;
+    var variable2 = req.params.variable2;
+    console.log("This was called!");
+    res.send(variable + ' ' + variable2);
+});
+
+app.listen(3000, '', function(){
+    console.log("App has started on ");
+});
